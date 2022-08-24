a32963d64c3a88a2330b66098eddf4d8769320e8
avats007 <84442028+avats007@users.noreply.github.com>
2022-08-24 09:34:30 +0530

    Create Dashboard.java

---
+++ Hotel-Management-System-2022/Dashboard.java
@@ -0,0 +1,89 @@
+package hotel.management.system;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+public class Dashboard extends JFrame implements ActionListener{
+    Dashboard()
+    {
+       
+       ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("icons/third.jpg"));
+       Image i2 = i1.getImage().getScaledInstance(1550, 1000, Image.SCALE_DEFAULT);
+       ImageIcon i3 = new ImageIcon(i2);
+       JLabel image = new JLabel(i3);
+       image.setBounds(0,0,1550,1000);
+       add(image);
+       
+       JLabel text = new JLabel("THE TAJ GROUP WELCOMES YOU"); 
+       text.setFont(new Font("Tahoma",Font.PLAIN,46));
+       text.setBounds(400,80,1000,50);
+       text.setForeground(Color.WHITE);
+       image.add(text);
+       
+       JMenuBar mb = new JMenuBar();
+       setBounds(0,0,1550,30);
+       image.add(mb);
+       
+       JMenu hotel = new JMenu("HOTEL MANAGEMENT");
+       hotel.setForeground(Color.red);
+       mb.add(hotel);
+       
+       JMenuItem reception = new JMenuItem("RECEPTION");
+       reception.addActionListener(this);
+       reception.setForeground(Color.magenta);
+       hotel.add(reception);
+       
+       JMenu admin = new JMenu("ADMIN");
+       admin.setForeground(Color.BLUE);
+       mb.add(admin);
+       
+       JMenuItem addemployee = new JMenuItem("ADD EMPLOYEE");
+       addemployee.addActionListener(this);
+       addemployee.setForeground(Color.magenta);
+       admin.add(addemployee);
+       
+       JMenuItem addrooms = new JMenuItem("ADD ROOMS");
+       addrooms.addActionListener(this);
+       addrooms.setForeground(Color.magenta);
+       admin.add(addrooms);
+       
+       JMenuItem adddrivers = new JMenuItem("ADD DRIVERS");
+       adddrivers.setForeground(Color.magenta);
+       adddrivers.addActionListener(this);
+       admin.add(adddrivers);
+       
+       setJMenuBar(mb);
+       
+       setDefaultCloseOperation(EXIT_ON_CLOSE);
+       setLayout(null);
+       setBounds(0,0,1550,1000);
+       
+       setVisible(true);
+    }
+    
+    @Override
+    public void actionPerformed(ActionEvent ae)
+    {
+        if(ae.getActionCommand().equals("ADD EMPLOYEE"))
+        {
+            new AddEmployee();
+        }
+        
+        else if(ae.getActionCommand().equals("ADD ROOMS"))
+        {
+            new AddRooms();
+        }
+        else if(ae.getActionCommand().equals("ADD DRIVERS"))
+        {
+            new AddDriver();
+        }
+        else if(ae.getActionCommand().equals("RECEPTION"))
+        {
+            new Reception();
+        }
+        
+    }
+    public static void main(String[]args)
+    {
+       new Dashboard(); 
+    }
+}
